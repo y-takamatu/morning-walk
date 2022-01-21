46a2bec891d8e90b245fbcd24a69e0d3350ec1e6
yuhi-ta <yuhi1307@gmail.com>
2022-01-21 13:05:25 +0900

    詳細画面実装

--- app/controllers/walks_controller.rb
+++ app/controllers/walks_controller.rb
@@ -11,6 +11,10 @@ class WalksController < ApplicationController
     Walk.create(walk_params)
   end
 
+  def show
+    @walk = Walk.find(params[:id])
+  end
+
   private
   def walk_params
     params.require(:walk).permit(:time,:content,:image).merge(user_id: current_user.id)
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'walks#index'
-  resources :walks,only: [:create,:index,:new]
+  resources :walks,only: [:create,:index,:new,:show]
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
