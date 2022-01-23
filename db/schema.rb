e910488778a09838908d1917881edcf2d1f67ca5
yuhi-ta <yuhi1307@gmail.com>
2022-01-23 12:39:56 +0900

    simpleカレンダー導入

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_22_074905) do
+ActiveRecord::Schema.define(version: 2022_01_23_030319) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -62,6 +62,7 @@ ActiveRecord::Schema.define(version: 2022_01_22_074905) do
     t.bigint "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.datetime "start_time"
     t.index ["user_id"], name: "index_walks_on_user_id"
   end
 
