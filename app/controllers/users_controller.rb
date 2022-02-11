9cd63f47962882cafe4d91801928c7e42d85a29e
yuhi-ta <yuhi1307@gmail.com>
2022-02-11 12:53:58 +0900

    トップ画面修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,10 @@
 class UsersController < ApplicationController
   def show
+    
+
     @user = User.find(params[:id])
+    @this_month =@user.walks.group("MONTH(created_at)")
+
     @walks = @user.walks
   end
 end
--- app/models/walk.rb
+++ app/models/walk.rb
@@ -7,7 +7,7 @@ class Walk < ApplicationRecord
   with_options presence: true do
     validates :time
     validates :content
-    validates :start_time, uniqueness: true
+    validates :start_time, uniqueness: { scope: :user_id }
   end
 
 end
