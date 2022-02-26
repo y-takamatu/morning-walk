7f281a0059a0218e2e579a1a7312d31432b675b2
yuhi-ta <yuhi1307@gmail.com>
2022-02-26 14:36:24 +0900

    タイムゾーン修正

--- config/application.rb
+++ config/application.rb
@@ -10,18 +10,7 @@ module MorningWalk
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
-    config.time_zone = ENV["TZ"]
-
-    # データベースの読み書きに使用するタイムゾーン(:local | :utc(default))
-    config.active_record.default_timezone = :utc
-
-    # i18nで使われるデフォルトのロケールファイルの指定(default :en)
-    config.i18n.default_locale = :ja
-
-    # $LOAD_PATHにautoload pathを追加しない(Zeitwerk有効時false推奨)
-    config.add_autoload_paths_to_load_path = false
-
-    config.api_only = true
+    
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
